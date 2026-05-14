17da72e6fbc4560a9ad022076e5e21ef1686d8d0
Cris <cristinaporteiro@gmail.com>
2026-05-14 16:22:22 +0200

    Migrated content to the HomePage component. Added routes using react-router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
-import CharacterList from './components/CharacterList/CharacterList';
-import Filters from './components/Filters/Filters';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import HomePage from './components/Pages/HomePage/HomePage';
 // import fetchData from '/services/api';
 
 function App() {
@@ -11,15 +11,9 @@ function App() {
     <>
       <Header />
       <main className="main">
-        <header className="hero central-column">
-          <h2 className="hero__title">
-            <span className="hero__title--big">C</span>haracter{' '}
-            <span className="hero__title--big">D</span>irectory
-          </h2>
-          <p className="hero__text">Explore the wizarding world</p>
-        </header>
-        <Filters />
-        <CharacterList />
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
       </main>
       <Footer />
     </>
--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
 import './HomePage.scss';
-
+import CharacterList from '../../CharacterList/CharacterList';
+import Filters from '../../Filters/Filters';
 const HomePage = () => {
-  return <div className="component">HomePage</div>;
+  return (
+    <>
+      <header className="hero central-column">
+        <h2 className="hero__title">
+          <span className="hero__title--big">C</span>haracter{' '}
+          <span className="hero__title--big">D</span>irectory
+        </h2>
+        <p className="hero__text">Explore the wizarding world</p>
+      </header>
+      <Filters />
+      <CharacterList />;
+    </>
+  );
 };
 
 export default HomePage;
